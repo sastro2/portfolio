24f09bf0fec44a117bb08f4acc88cef8855846df
Sascha Stropek <sascha.stropek@gmail.com>
2022-05-03 07:15:11 +0200

    Change personal details

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -22,8 +22,8 @@ function Contact() {
               labore anim adipisicing amet irure tempor.
             </p>
             <ul className="contact-links">
-              <li className="contact-item">Email: lutfhi@me.com</li>
-              <li className="contact-item">Phone: +6212344321007</li>
+              <li className="contact-item">Email: sascha.stropek@gmail.com</li>
+              <li className="contact-item">Phone: +43 6801545211</li>
             </ul>
           </div>
         </div>
